2bc4cd792fcb946f639685c136e4244e8d27aa1b
chuckschuldinerdeath <146342080+chuckschuldinerdeath@users.noreply.github.com>
2023-09-28 23:56:07 +0500

    Initial commit

---
+++ src/main/java/ru/otus/murat/first/project/MainApplication.java
@@ -0,0 +1,19 @@
+package ru.otus.murat.first.project;
+import java.util.Scanner;
+
+public class MainApplication {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("введите цифру: ");
+        int num = in.nextInt();
+
+        if(num > 7){
+            System.out.println("Привет, Вячеслав!");
+        }
+        else {
+            System.out.println("Ваша цифра не больше 7");
+        }
+    }
+}
